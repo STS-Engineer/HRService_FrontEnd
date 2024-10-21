20fa0d7428c86e7fefa4feafa953731835eb2bfe
STS-Engineer <administration.sts@avocarbon.com>
2024-10-21 14:51:27 +0100

    Update MissionAdmin.js

--- src/Components/MissionAdmin.js
+++ src/Components/MissionAdmin.js
@@ -22,7 +22,7 @@ const MissionAdmin = () => {
     const fetchMissionRequests = () => {
       const token = localStorage.getItem("token");
       fetch(
-        `http://bhr-avocarbon.azurewebsites.net/mission-requests?page=${currentPage}&limit=${requestsPerPage}`,
+        `https://bhr-avocarbon.azurewebsites.net/mission-requests?page=${currentPage}&limit=${requestsPerPage}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -57,7 +57,7 @@ const MissionAdmin = () => {
     }).then((result) => {
       if (result.isConfirmed) {
         const token = localStorage.getItem("token");
-        fetch(`http://bhr-avocarbon.azurewebsites.net/mission-requests/${id}`, {
+        fetch(`https://bhr-avocarbon.azurewebsites.net/mission-requests/${id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
@@ -104,7 +104,7 @@ const MissionAdmin = () => {
     }).then((result) => {
       if (result.isConfirmed) {
         const token = localStorage.getItem("token");
-        fetch(`http://bhr-avocarbon.azurewebsites.net/mission-requests/${id}`, {
+        fetch(`https://bhr-avocarbon.azurewebsites.net/mission-requests/${id}`, {
           method: "DELETE",
           headers: {
             "Content-Type": "application/json",
