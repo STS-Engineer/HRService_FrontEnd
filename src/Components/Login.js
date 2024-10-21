674eb02d88a36ca09cab4d6e4e125dfebbc03e1b
STS-Engineer <administration.sts@avocarbon.com>
2024-10-21 14:50:49 +0100

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -36,7 +36,7 @@ const Login = () => {
 
   const handleSubmit = async (values) => {
     try {
-      const response = await axios.post("http://bhr-avocarbon.azurewebsites.net/auth/login", {
+      const response = await axios.post("https://bhr-avocarbon.azurewebsites.net/auth/login", {
         email: values.username,
         password: values.password,
         plant_connection: values.plantConnection,
