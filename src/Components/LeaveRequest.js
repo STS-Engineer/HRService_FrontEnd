d213bd0639d28b0237b64a24b9fd0ab8b4c6cfca
STS-Engineer <administration.sts@avocarbon.com>
2024-10-21 14:50:08 +0100

    Update LeaveRequest.js

--- src/Components/LeaveRequest.js
+++ src/Components/LeaveRequest.js
@@ -37,7 +37,7 @@ const LeaveRequest = ({ employeeId }) => {
 
     try {
       await axios.post(
-        "http://bhr-avocarbon.azurewebsites.net/leave-requests",
+        "https://bhr-avocarbon.azurewebsites.net/leave-requests",
         newLeaveRequest,
         {
           headers: {
