7d5197c3235a9ac86b26e2d3614eec9ecf43d51a
STS-Engineer <administration.sts@avocarbon.com>
2024-10-21 13:54:48 +0100

    Update ChangePassword.js

--- src/Components/ChangePassword.js
+++ src/Components/ChangePassword.js
@@ -31,7 +31,7 @@ const ChangePassword = () => {
     try {
       const token = localStorage.getItem("token");
       const response = await fetch(
-        "http://localhost:3000/auth/update-password/",
+        "http://bhr-avocarbon.azurewebsites.net/auth/update-password/",
         {
           method: "PATCH",
           headers: {
